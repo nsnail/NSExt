f846b7bdbf908ca23e580fbea3d81025863de25e
nsnail <taokeu@gmail.com>
2023-01-29 20:32:58 +0800

    调整一下日志格式

--- src/Extensions/LoggerExtensions.cs
+++ src/Extensions/LoggerExtensions.cs
@@ -8,27 +8,27 @@ namespace NSExt.Extensions;
 public static class LoggerExtensions
 {
     private const string _MESSAGE_S_THREADID_CALLERNAME_CALLERFILEPATH_CALLERLINENUMBER
-        = "{Message} <s:{ThreadId}#{CallerName}@{CallerFilePath}:{CallerLineNumber}>";
+        = "{Message} <s:{CallerName}@{CallerFilePath}:{CallerLineNumber}>";
 
-    private static readonly Action<ILogger, string, string, string, string, string, Exception> _logDebug
-        = LoggerMessage.Define<string, string, string, string, string>(LogLevel.Debug, default
-                                                                     , _MESSAGE_S_THREADID_CALLERNAME_CALLERFILEPATH_CALLERLINENUMBER);
+    private static readonly Action<ILogger, string, string, string, string, Exception> _logDebug
+        = LoggerMessage.Define<string, string, string, string>(LogLevel.Debug, default
+                                                             , _MESSAGE_S_THREADID_CALLERNAME_CALLERFILEPATH_CALLERLINENUMBER);
 
-    private static readonly Action<ILogger, string, string, string, string, string, Exception> _logError
-        = LoggerMessage.Define<string, string, string, string, string>(LogLevel.Error, default
-                                                                     , _MESSAGE_S_THREADID_CALLERNAME_CALLERFILEPATH_CALLERLINENUMBER);
+    private static readonly Action<ILogger, string, string, string, string, Exception> _logError
+        = LoggerMessage.Define<string, string, string, string>(LogLevel.Error, default
+                                                             , _MESSAGE_S_THREADID_CALLERNAME_CALLERFILEPATH_CALLERLINENUMBER);
 
-    private static readonly Action<ILogger, string, string, string, string, string, Exception> _logFatal
-        = LoggerMessage.Define<string, string, string, string, string>(LogLevel.Critical, default
-                                                                     , _MESSAGE_S_THREADID_CALLERNAME_CALLERFILEPATH_CALLERLINENUMBER);
+    private static readonly Action<ILogger, string, string, string, string, Exception> _logFatal
+        = LoggerMessage.Define<string, string, string, string>(LogLevel.Critical, default
+                                                             , _MESSAGE_S_THREADID_CALLERNAME_CALLERFILEPATH_CALLERLINENUMBER);
 
-    private static readonly Action<ILogger, string, string, string, string, string, Exception> _logInfo
-        = LoggerMessage.Define<string, string, string, string, string>(LogLevel.Information, default
-                                                                     , _MESSAGE_S_THREADID_CALLERNAME_CALLERFILEPATH_CALLERLINENUMBER);
+    private static readonly Action<ILogger, string, string, string, string, Exception> _logInfo
+        = LoggerMessage.Define<string, string, string, string>(LogLevel.Information, default
+                                                             , _MESSAGE_S_THREADID_CALLERNAME_CALLERFILEPATH_CALLERLINENUMBER);
 
-    private static readonly Action<ILogger, string, string, string, string, string, Exception> _logWarn
-        = LoggerMessage.Define<string, string, string, string, string>(LogLevel.Warning, default
-                                                                     , _MESSAGE_S_THREADID_CALLERNAME_CALLERFILEPATH_CALLERLINENUMBER);
+    private static readonly Action<ILogger, string, string, string, string, Exception> _logWarn
+        = LoggerMessage.Define<string, string, string, string>(LogLevel.Warning, default
+                                                             , _MESSAGE_S_THREADID_CALLERNAME_CALLERFILEPATH_CALLERLINENUMBER);
 
     /// <summary>
     ///     Debug
@@ -36,9 +36,8 @@ public static class LoggerExtensions
     public static void Debug(this             ILogger me, object message, [CallerMemberName] string callerName = null
                            , [CallerFilePath] string callerFilePath = null, [CallerLineNumber] int callerLineNumber = 0)
     {
-        _logDebug(me, message.ToString(), Environment.CurrentManagedThreadId.ToString(CultureInfo.InvariantCulture)
-                , callerName, Path.GetFileName(callerFilePath), callerLineNumber.ToString(CultureInfo.InvariantCulture)
-                , null);
+        _logDebug(me, message.ToString(), callerName, Path.GetFileName(callerFilePath)
+            ,         callerLineNumber.ToString(CultureInfo.InvariantCulture), null);
     }
 
     /// <summary>
@@ -47,9 +46,8 @@ public static class LoggerExtensions
     public static void Error(this             ILogger me, object message, [CallerMemberName] string callerName = null
                            , [CallerFilePath] string callerFilePath = null, [CallerLineNumber] int callerLineNumber = 0)
     {
-        _logError(me, message.ToString(), Environment.CurrentManagedThreadId.ToString(CultureInfo.InvariantCulture)
-                , callerName, Path.GetFileName(callerFilePath), callerLineNumber.ToString(CultureInfo.InvariantCulture)
-                , null);
+        _logError(me, message.ToString(), callerName, Path.GetFileName(callerFilePath)
+            ,         callerLineNumber.ToString(CultureInfo.InvariantCulture), null);
     }
 
     /// <summary>
@@ -59,9 +57,8 @@ public static class LoggerExtensions
                            , [CallerMemberName] string  callerName = null, [CallerFilePath] string callerFilePath = null
                            , [CallerLineNumber] int     callerLineNumber = 0)
     {
-        _logFatal(me, message.ToString(), Environment.CurrentManagedThreadId.ToString(CultureInfo.InvariantCulture)
-                , callerName, Path.GetFileName(callerFilePath), callerLineNumber.ToString(CultureInfo.InvariantCulture)
-                , ex);
+        _logFatal(me, message.ToString(), callerName, Path.GetFileName(callerFilePath)
+            ,         callerLineNumber.ToString(CultureInfo.InvariantCulture), ex);
     }
 
     /// <summary>
@@ -70,9 +67,8 @@ public static class LoggerExtensions
     public static void Info(this             ILogger me, object message, [CallerMemberName] string callerName = null
                           , [CallerFilePath] string  callerFilePath = null, [CallerLineNumber] int callerLineNumber = 0)
     {
-        _logInfo(me, message.ToString(), Environment.CurrentManagedThreadId.ToString(CultureInfo.InvariantCulture)
-               , callerName, Path.GetFileName(callerFilePath), callerLineNumber.ToString(CultureInfo.InvariantCulture)
-               , null);
+        _logInfo(me, message.ToString(), callerName, Path.GetFileName(callerFilePath)
+           ,         callerLineNumber.ToString(CultureInfo.InvariantCulture), null);
     }
 
     /// <summary>
@@ -81,8 +77,7 @@ public static class LoggerExtensions
     public static void Warn(this             ILogger me, object message, [CallerMemberName] string callerName = null
                           , [CallerFilePath] string  callerFilePath = null, [CallerLineNumber] int callerLineNumber = 0)
     {
-        _logWarn(me, message.ToString(), Environment.CurrentManagedThreadId.ToString(CultureInfo.InvariantCulture)
-               , callerName, Path.GetFileName(callerFilePath), callerLineNumber.ToString(CultureInfo.InvariantCulture)
-               , null);
+        _logWarn(me, message.ToString(), callerName, Path.GetFileName(callerFilePath)
+           ,         callerLineNumber.ToString(CultureInfo.InvariantCulture), null);
     }
 }
