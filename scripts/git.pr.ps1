e4958ef065f4295af8a2139c0d27586148555f51
tk <taokeu@gmail.com>
2023-12-14 14:03:48 +0800

    ci: 🎡 发版流水线脚本

--- scripts/git.pr.ps1
+++ scripts/git.pr.ps1
@@ -5,7 +5,7 @@ git add ../
 ../node_modules/.bin/git-cz.ps1
 git pull
 git push --set-upstream origin $branch
-Start-Process -FilePath "https://github.com/nsnail/dot/compare/main...$branch"
+Start-Process -FilePath "https://github.com/nsnail/NSExt/compare/main...$branch"
 Write-Host "按『Enter』重建分支，『Ctrl+C』退出"
 Pause
 ./git.rc.ps1
