cc703816caabde68ccc62e1b5d4563d81f614a23
nsnail <taokeu@gmail.com>
2023-12-14 16:46:09 +0800

    build: 📦 nuget 构建相关 (#4)

--- scripts/code.clean.ps1
+++ scripts/code.clean.ps1
@@ -1,4 +1,4 @@
-dotnet jb cleanupcode --no-build --include = $( $( git status --porcelain | Where-Object { $_ -match "^\s*[MA]" } | ForEach-Object { $_.TrimStart(" M").TrimStart(" A") } ) -join ";" ) ../NSExt.sln
+jb cleanupcode --no-build --include = $( $( git status --porcelain | Where-Object { $_ -match "^\s*[MA]" } | ForEach-Object { $_.TrimStart(" M").TrimStart(" A") } ) -join ";" ) ../NSExt.sln
 dot rbom -w -e refs -e .git -e node_modules ../
 dot trim -w -e refs -e .git -e node_modules ../
 dot tolf -w -e refs -e .git -e node_modules ../
