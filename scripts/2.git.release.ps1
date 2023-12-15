51063bacfe2a2a47144f5c8d104c7bb75e6a9e8e
nsnail <taokeu@gmail.com>
2023-12-15 16:31:44 +0800

    Tk (#15)
    * chore(release): 2.0.3
    
    * build: 📦 构建脚本调整

---
+++ scripts/2.git.release.ps1
@@ -0,0 +1,23 @@
+cd ..
+$types = @{
+    '1' = @('major', '主版本')
+    '2' = @('minor', '此版本')
+    '3' = @('patch', '修订版本')
+}
+$prefix = ''
+while ($null -eq $types[$prefix])
+{
+    $prefix = Read-Host "请选择版本类型`n" $( & { param($i) $i | ForEach-Object { "$_ : $( $types[$_][0] )（$( $types[$_][1] )）`n" } } $types.Keys | Sort-Object )
+}
+git checkout main
+git branch -D release
+git checkout -b release
+./node_modules/.bin/standard-version -r $types[$prefix][0]
+git push --follow-tags --force origin release
+Start-Process -FilePath "https://github.com/nsnail/NSExt/compare/main...release"
+cd ./scripts
+Write-Host "按『Enter』回到主分支，『Ctrl+C』退出"
+Pause
+git checkout main
+git pull
+git branch -D release
