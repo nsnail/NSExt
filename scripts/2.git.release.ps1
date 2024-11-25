b32fe597ddc0202f313dec90c1b435ac36dff10f
nsnail <taokeu@gmail.com>
2024-11-25 17:01:15 +0800

    perf: ⚡ 支持.net9 (#21)
    Co-authored-by: tk <fiyne1a@dingtalk.com>

--- scripts/2.git.release.ps1
+++ scripts/2.git.release.ps1
@@ -1,7 +1,7 @@
 cd ..
 $types = @{
     '1' = @('major', '主版本')
-    '2' = @('minor', '此版本')
+    '2' = @('minor', '次版本')
     '3' = @('patch', '修订版本')
 }
 $prefix = ''
@@ -10,19 +10,20 @@ while ($null -eq $types[$prefix])
     $prefix = Read-Host "请选择版本类型`n" $( & { param($i) $i | ForEach-Object { "$_ : $( $types[$_][0] )（$( $types[$_][1] )）`n" } } $types.Keys | Sort-Object )
 }
 git checkout main
+git pull
 git branch -D release
 git checkout -b release
 ./node_modules/.bin/standard-version -r $types[$prefix][0]
-cd ./scripts
-./code.clean.ps1
 git commit --amend --no-edit -a
-$tag = $(git describe --tags $(git rev-list --tags --max-count=1))
+$tag = $( git describe --tags $( git rev-list --tags --max-count = 1 ) )
 git tag -d $tag
 git tag $tag
 git push --tags origin release
 Start-Process -FilePath "https://github.com/nsnail/NSExt/compare/main...release"
-Write-Host "按『Enter』回到主分支，『Ctrl+C』退出"
+Write-Host "按『Enter』回到tk分支，『Ctrl+C』退出"
 Pause
 git checkout main
 git pull
 git branch -D release
+git branch -D tk
+git checkout -b tk
--- src/backend/GlobalUsings.cs
+++ src/backend/GlobalUsings.cs
@@ -1,5 +1,6 @@
 global using System;
 global using System.ComponentModel;
+global using System.Diagnostics;
 global using System.Globalization;
 global using System.Text;
 global using System.Text.RegularExpressions;
